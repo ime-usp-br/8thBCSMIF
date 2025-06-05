87255262f2be9918b212e5e1c4a8201a31966651
Ayrton Douglas Freitas Guimarães <ayrtondouglas@usp.br>
2025-06-05 15:18:17 -0300

    feat(mail): Implementa view Markdown com saudação e resumo de inscrição AC2 (#8)
    - Adiciona saudação personalizada com nome do usuário (full_name)
    - Lista eventos selecionados com preços individuais da tabela pivot
    - Exibe valor total formatado da inscrição (calculated_fee)
    - Inclui status do pagamento e próximos passos
    - Formatação monetária brasileira (R$ X.XXX,XX) para valores
    - Condicional para exibir instruções diferentes baseadas na taxa
    - Atende AC2: View Markdown para usuário contém saudação, resumo da inscrição (eventos, valor total)

--- resources/views/emails/registration/new.blade.php
+++ resources/views/emails/registration/new.blade.php
@@ -1,12 +1,31 @@
 <x-mail::message>
-# Introduction
+# Confirmação de Inscrição - 8th BCSMIF
 
-The body of your message.
+Olá {{ $registration->full_name }},
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
+Agradecemos sua inscrição para o 8º Congresso Brasileiro de Modelagem Estatística em Finanças e Seguros (8th BCSMIF)!
 
-Thanks,<br>
-{{ config('app.name') }}
+## Resumo da sua Inscrição
+
+**Eventos Selecionados:**
+@foreach($registration->events as $event)
+- {{ $event->name }}: R$ {{ number_format((float) $event->pivot->price_at_registration, 2, ',', '.') }}
+@endforeach
+
+**Valor Total:** R$ {{ number_format((float) $registration->calculated_fee, 2, ',', '.') }}
+
+@if($registration->calculated_fee > 0)
+**Status do Pagamento:** {{ ucfirst(str_replace('_', ' ', $registration->payment_status)) }}
+
+Em breve você receberá instruções detalhadas sobre como proceder com o pagamento.
+@else
+**Status do Pagamento:** Isento de taxa
+@endif
+
+## Próximos Passos
+
+Mantenha este e-mail para seus registros. Entraremos em contato em breve com mais informações sobre o evento.
+
+Atenciosamente,<br>
+Organização do {{ config('app.name') }}
 </x-mail::message>
