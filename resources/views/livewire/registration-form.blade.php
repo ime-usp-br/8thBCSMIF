f1e573ca94071b2cbe27fc8eaacc4304093fb9eb
ayrtonnotrya <ayrtondouglas@usp.br>
2025-08-02 12:18:57 -0300

    feat(ui): Add help section to registration form
    - Adds a new contact information section to the registration form.
    - This section provides a direct email link to the event coordinator for user support.
    - Includes translations for both English and Brazilian Portuguese.
    
    Ref: #87

--- resources/views/livewire/registration-form.blade.php
+++ resources/views/livewire/registration-form.blade.php
@@ -997,7 +997,22 @@ new #[Layout('layouts.app')] class extends Component {
                         </div>
                     </div>
 
-
+                    {{-- Contact Information --}}
+                    <div class="bg-blue-50 dark:bg-blue-900/20 border border-blue-200 dark:border-blue-800 rounded-lg p-4 mb-6">
+                        <div class="flex items-start">
+                            <svg class="w-5 h-5 text-blue-600 dark:text-blue-400 mt-0.5 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
+                            </svg>
+                            <div class="text-sm text-blue-800 dark:text-blue-200">
+                                <p class="font-medium mb-1">{{ __('Need Help?') }}</p>
+                                <p>{{ __('If you have any doubt or inconvenience during the registration, please send an email to') }} 
+                                    <a href="mailto:{{ config('mail.coordinator_email') }}" class="font-medium underline hover:no-underline">
+                                        {{ config('mail.coordinator_email') }}
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
                     {{-- Submit Button --}}
                     <div class="flex flex-col sm:flex-row justify-center sm:justify-end space-y-3 sm:space-y-0 sm:space-x-4 pt-4">
